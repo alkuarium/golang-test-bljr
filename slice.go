dd0de2b5a268cb4691586661512b17d1217638f9
unknown <rafialkhawarizmi321@gmail.com>
2026-09-24 21:12:04 +0700

    Add slice.go

---
+++ slice.go
@@ -0,0 +1,52 @@
+package main 
+
+import "fmt"
+
+func Array() {
+
+	// SLICE
+	fmt.Println("---SLICE---")
+
+	buah := []string{"Mangga", "Apel", "Jeruk", "Pisang", "Semangka"}
+
+	a := buah[:1]
+	b := buah[2:5]
+	buah[0] = "Durian"
+	addbuah := append(buah, "Nanas", "Melon")
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(addbuah)
+
+
+	//ARRAY BIASA
+	data := []int{1, 2, 3, 4, 5}
+	fmt.Println(data)
+
+	dataBuah := []string{"Mangga", "Apel", "Jeruk", "Pisang", "Semangka"}
+	fmt.Println(dataBuah)
+	aku := len(dataBuah)
+	fmt.Println(aku)
+
+
+
+	//MAP
+	fmt.Println("---MAP---")
+
+	car := map[string]string{
+		"Car1":  "Toyota",
+		"Car2":  "Honda",
+		"Car3":  "Suzuki",
+		"Car4":  "Mitsubishi",
+		"Car5":  "Daihatsu",
+	}
+
+
+	car["Car3"] = "yamaha"
+	delete(car, "Car5")
+	hitngCAR := len(car)
+
+	fmt.Println(car)
+	fmt.Println(hitngCAR)
+	fmt.Println(car["Car4"])
+}
